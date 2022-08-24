b5755a236f6ae28cde9a8b6ce9f799bb2db48b4c
Natalia Godoy <Na.godoy@duocuc.cl>
2022-08-24 19:07:02 -0400

    animacion

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -8,38 +8,38 @@ import { AnimationController } from '@ionic/angular';
 })
 export class HomePage implements OnInit {
 
-  input: string 
+  input: string;
 
   constructor(private anim: AnimationController) {}
 
   validar(){
-    if(this.input == "" || this.input == null){
+    if(this.input == '' || this.input == null){
     this.anim.create()
-    .addElement(document.querySelector("#input"))
+    .addElement(document.querySelector('#input'))
     .duration(100)
     .iterations(3)
-    .fromTo("transform", "translateX(-10px)", "translateX(0px)")
-    .fromTo("border", "1px red solid", "1px black solid")
-    .fromTo("background","red","white")
-    .fromTo("background","red","white")
+    .fromTo('transform', 'translateX(-10px)', 'translateX(0px)')
+    .fromTo('border', '1px red solid', '1px black solid')
+    .fromTo('background','red','white')
+    .fromTo('background','red','white')
     .play()
   }
 }
 
 ngOnInit(): void {
   this.anim.create()
-  .addElement(document.querySelector("img"))
+  .addElement(document.querySelector('img'))
   .duration(3000)
   .delay(1000)
   .keyframes([
-    {offset: 0.25, transform: "scale(0.8)"},
-    {offset: 0.35, transform: "scale(0.8) rotate(-20deg)"},
-    {offset: 0.45, transform: "scale(0.8) rotate(0deg)"},
-    {offset: 0.55, transform: "scale(0.8) rotate(20deg)"},
-    {offset: 0.65, transform: "scale(0.8) rotate(0deg)", opacity:1},
-    {offset: 1, transform: "scale(1)", opacity: 0}
+    {offset: 0.25, transform: 'scale(0.8)'},
+    {offset: 0.35, transform: 'scale(0.8) rotate(-20deg)'},
+    {offset: 0.45, transform: 'scale(0.8) rotate(0deg)'},
+    {offset: 0.55, transform: 'scale(0.8) rotate(20deg)'},
+    {offset: 0.65, transform: 'scale(0.8) rotate(0deg)', opacity:1},
+    {offset: 1, transform: 'scale(1)', opacity: 0}
     ])
-  .play()
+  .play();
 }
 
 }
